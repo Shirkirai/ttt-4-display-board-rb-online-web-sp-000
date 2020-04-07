efb144a5a23263a48832efaaba3109db14fdd718
Jung Lee <lee.jung.s95@gmail.com>
2020-04-07 03:53:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board = ["X", "X", "X", " ", "X", " ", " ", " ", " "])
+def display_board(board = ["X", "X", "X", " ", "X", " ", "O", "O", "O"])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "   | #{board[4]} |   "
