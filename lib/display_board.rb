5e019362aa48f0691bd0736e81dc4c9f637cc5cf
Jung Lee <lee.jung.s95@gmail.com>
2020-04-07 03:51:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
   puts "   |   |   "
   puts "-----------"
-  puts "   | #{board[5]} |   "
+  puts "   | #{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
